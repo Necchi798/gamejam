7035f779b196a11a3f3d00437ccb7a092ac9ff3f
Necchi798 <122287094+Necchi798@users.noreply.github.com>
2025-10-19 18:37:11 +0200

    added lights to signal doors opening

--- gamejam/scripts/door.gd
+++ gamejam/scripts/door.gd
@@ -2,10 +2,15 @@ class_name door
 extends Node2D
 @export var required_switches: int = 1
 var pressed_switchs = 0
-var open = false
+@export var open: bool = false
+@onready var red_switches =[$Red1,$Red2,$Red3]
+@onready var green_switches =[$Green,$Green2,$Green3]
+var greenAux 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+
+	for i in  required_switches: 
+		red_switches[i].set_deferred("visible",true)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,10 +18,14 @@ func _process(delta: float) -> void:
 	pass
 func switch_pressed():
 	pressed_switchs+=1
+	for i in  pressed_switchs: 
+		green_switches[i].visible = true
 	toggle()
 	
 func switch_released():
 	pressed_switchs -=1
+	for i in  (required_switches- pressed_switchs): 
+		green_switches[i].visible = false
 	toggle()
 	
 func toggle():
